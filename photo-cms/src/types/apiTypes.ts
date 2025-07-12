80493a189d3ff3764c75ddbd76c335f26bafe643
Leopold Blum <leopold.blum@gmx.de>
2025-07-12 18:16:06 +0200

    added types for apiResponse jsons, displaying single set now

---
+++ photo-cms/src/types/apiTypes.ts
@@ -0,0 +1,50 @@
+export interface ApiResponse {
+    docs: Doc[];
+    totalDocs: number;
+    limit: number;
+    totalPages: number;
+    page: number;
+    pagingCounter: number;
+    hasPrevPage: boolean;
+    hasNextPage: boolean;
+    prevPage: number | null;
+    nextPage: number | null;
+}
+
+export interface Doc {
+    createdAt: string;
+    updatedAt: string;
+    project: Project;
+    images: ImageWrapper[];
+    id: string;
+}
+
+export interface Project {
+    createdAt: string;
+    updatedAt: string;
+    title: string;
+    description: string;
+    date: string;
+    id: string;
+}
+
+export interface ImageWrapper {
+    image: Image;
+    id: string;
+}
+
+export interface Image {
+    createdAt: string;
+    updatedAt: string;
+    alt: string;
+    filename: string;
+    mimeType: string;
+    filesize: number;
+    width: number;
+    height: number;
+    focalX: number;
+    focalY: number;
+    id: string;
+    url: string;
+    thumbnailURL: string | null;
+}
