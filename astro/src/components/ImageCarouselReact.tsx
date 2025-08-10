8061b387526380e923d4c5688b6efe1c3027d9fe
Leopold Blum <leopold.blum@gmx.de>
2025-08-10 14:40:36 +0200

    finally fixed the layouts for imageCarousel and project pages

--- astro/src/components/ImageCarouselReact.tsx
+++ astro/src/components/ImageCarouselReact.tsx
@@ -1,57 +1,55 @@
 import { useState } from "react";
 import type { Photoset } from "../../../photo-cms/src/types/apiTypes";
-import { AnimatePresence, motion } from "motion/react"
+import { AnimatePresence, motion } from "motion/react";
 import { CustomCursor } from "./CustomCursor";
 
+const ArrowButton = ({ position, onClick, label }: { position: "left" | "right", onClick: () => void, label: string }) => (
+    <button
+        className={`absolute ${position}-0 w-1/3 z-10 h-full cursor-none invisible md:visible`}
+        onClick={onClick}
+        onMouseOver={() => CustomCursor.setCursorText(label)}
+        onMouseLeave={() => CustomCursor.setCursorText("")}
+    />
+);
 
+const sliderVariants = {
+    incoming: (direction: number) => ({
+        x: direction > 0 ? 200 : -200,
+        opacity: 0
+    }),
+    active: { x: 0, opacity: 1 },
+    exit: (direction: number) => ({
+        x: direction > 0 ? -200 : 200,
+        opacity: 0
+    })
+};
 
 const ImageCarouselReact = ({ photoSet }: { photoSet: Photoset }) => {
-
-    const [imageIndex, setImageIndex] = useState(0)
-    const [direction, setDirection] = useState(1)
+    const [imageIndex, setImageIndex] = useState(0);
+    const [direction, setDirection] = useState(1);
 
     const scrollLeft = () => {
-        setDirection(-1)
-
-        if (imageIndex > 0) setImageIndex(prev => prev - 1)
-        else setImageIndex(photoSet.images.length - 1)
-    }
+        setDirection(-1);
+        setImageIndex(prev => prev > 0 ? prev - 1 : photoSet.images.length - 1);
+    };
 
     const scrollRight = () => {
-        setDirection(1)
-
-        if (imageIndex < photoSet.images.length - 1) setImageIndex(prev => prev + 1)
-        else setImageIndex(0)
-    }
-
-    const sliderVariants = {
-        incoming: (direction: number) => ({
-            x: direction > 0 ? "200px" : "-200px",
-            opacity: 0
-        }),
-        active: { x: 0, opacity: 1 },
-        exit: (direction: number) => ({
-            x: direction > 0 ? "-200px" : "200px",
-            opacity: 0
-        })
-    }
-
+        setDirection(1);
+        setImageIndex(prev => prev < photoSet.images.length - 1 ? prev + 1 : 0);
+    };
 
     return (
-        <div className="py-20 w-full h-full">
+        <div className="flex flex-col justify-center items-center relative cursor-none w-full">
+            <ArrowButton position="left" onClick={scrollLeft} label="<" />
+            <ArrowButton position="right" onClick={scrollRight} label=">" />
 
-            {/* carousel */}
-            <div className="flex items-center justify-center relative overflow-x-clip w-full h-full">
 
-                <button className="absolute h-full w-3/7 left-0 z-10 cursor-none invisible md:visible"
-                    onClick={scrollLeft}
-                    onMouseOver={() => CustomCursor.setCursorText("<")}
-                    onMouseLeave={() => CustomCursor.setCursorText("")}
-                />
+            <div className="flex items-center justify-center h-[70vh]">
 
                 <AnimatePresence mode="wait" initial={false} custom={direction}>
                     <motion.img
-                        className="object-contain w-full h-full z-0 cursor-none"
+                        className="object-contain flex-1 max-h-[70vh]"
+
                         loading="eager"
                         key={photoSet.images[imageIndex].image.url}
                         src={`http://localhost:3001/${photoSet.images[imageIndex].image.url}`}
@@ -62,42 +60,23 @@ const ImageCarouselReact = ({ photoSet }: { photoSet: Photoset }) => {
                         animate="active"
                         exit="exit"
                         transition={{ duration: 0.2, ease: "easeInOut" }}
-
                         drag="x"
                         dragConstraints={{ left: 0, right: 0 }}
-                        onDragEnd={(e, info) => {
-                            if (info.offset.x < 100) scrollRight();
-                            else if (info.offset.x > -100) scrollLeft();
-                        }
-                        }
-
+                        onDragEnd={(e, { offset }) => {
+                            if (offset.x < -100) scrollRight();
+                            else if (offset.x > 100) scrollLeft();
+                        }}
                         onMouseOver={() => CustomCursor.setCursorText("+")}
-                        onMouseLeave={() => CustomCursor.setCursorText("")}
+                        onClick={() => console.log("open popup")}
                     />
                 </AnimatePresence>
-
-                <button className="absolute h-full w-3/7 right-0 z-10 cursor-none invisible md:visible"
-                    onClick={scrollRight}
-                    onMouseOver={() => CustomCursor.setCursorText(">")}
-                    onMouseLeave={() => CustomCursor.setCursorText("")}
-                />
-
             </div>
 
-            {/* counter */}
-            <div className="pt-3 font-extrabold flex justify-center items-center">
-                <AnimatePresence mode="wait" custom={direction}>
-                    <div>
-                        {`${imageIndex + 1} / ${photoSet.images.length}`}
-                    </div>
-                </AnimatePresence>
+            <div className="py-2">
+                {`${imageIndex + 1} / ${photoSet.images.length}`}
             </div>
-
         </div>
+    );
+};
 
-    )
-}
-
-
-
-export default ImageCarouselReact
+export default ImageCarouselReact;
